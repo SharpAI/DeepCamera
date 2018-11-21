5a8cc49fab8808a2b975df1f27cfa4a625924a3f
Simba Zhang <solderzzc@gmail.com>
2018-11-21 00:25:44 +0000

    commit scripts

---
+++ build/build_aarch64.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+rm -rf build dist runtime
+mkdir runtime
+pyinstaller classifier_server.spec
+mv dist/classifier runtime/
+pyinstaller embedding.spec
+cp -rf dist/embedding/* runtime/
+rm -rf dist/embedding
+pyinstaller face_detector.spec
+cp -rf dist/worker/* runtime/
+rm -rf dist/worker
+pyinstaller parameter_server.spec
+cp -rf dist/param/* runtime/
+rm -rf dist/param
+
+mkdir -p runtime/data/faces
+mkdir runtime/faces/default_data
+mkdir runtime/image
+cp ../src/embedding/data/data.sqlite ./runtime/data/
+cp ../src/embedding/data/params.ini ./runtime/data/
+cp ../src/embedding/faces/default_data/default_face.png ./runtime/faces/default_data/
+cp ../src/embedding/image/Mike*.png ./runtime/image
+cp -rf ../src/embedding/pages ./runtime/
+cp -rf ../src/embedding/migrations ./runtime/
+
+cp scripts/*_aarch64.sh runtime/
+chmod +x runtime/*.sh
