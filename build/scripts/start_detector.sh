9de78f07c0a1971b69564ea57d9583ec6d7d2e93
Simba Zhang <solderzzc@gmail.com>
2018-11-22 01:00:48 +0000

    To add detector build script.

---
+++ build/scripts/start_detector.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DEVICE_UUID_FILEPATH=/data/data/com.termux/files/home/.ro_serialno
+export DEVICE_GROUP_ID_FILEPATH=/data/data/com.termux/files/home/.groupid.txt
+
+while [ 1 ]
+do
+  REDIS_HOST=localhost FLOWER_ADDRESS=localhost FLOWER_PORT=5556 node main.bin.js
+  sleep 20
+done
+
+
