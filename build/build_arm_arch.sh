2ea37a927a49c42e4ac9d7df35d9d5f33c443798
Simba Zhang <solderzzc@gmail.com>
2018-12-01 02:01:06 +0000

    Removed classifier_single_thread_server.spec. Added build script for arch linux.

--- build/build_arm_arch.sh
+++ build/build_arm_arch.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-rm -rf build dist runtime
-mkdir runtime
-pyinstaller classifier_single_thread_server.spec
-mv dist/classifier runtime/bin
+runtime_tar=$1/./runtime_arch.tar
+
+rm -rf build dist runtime_arch
+mkdir runtime_arch
+
+pyinstaller -y face_detector_arm.spec
+mv dist/worker runtime_arch/bin
+cp -rf ../src/face_detection/model runtime_arch/bin/
+
+rm $runtime_tar
+tar -cf $runtime_tar runtime_arch
+
+rm -rf runtime_arch/bin
+
+pyinstaller -y classifier_server.spec
+mv dist/classifier runtime_arch/bin
+
+tar -uf $runtime_tar runtime_arch
+
+echo "$runtime_tar is ready"
