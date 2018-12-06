25430dccb5c51b62331c005a4df5054f315854a1
Simba Zhang <solderzzc@users.noreply.github.com>
2018-12-06 11:33:05 -0800

    Update worker_arm.sh

--- build/scripts/worker_arm.sh
+++ build/scripts/worker_arm.sh
@@ -2,6 +2,6 @@
 cd bin
 while [ 1 ]
 do
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib LD_PRELOAD=libm.so:libatomic.so:libcutils.so:libpython2.7.so.1.0 ./worker worker --loglevel INFO -E -n detect -c 2 -Q detect
+  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib LD_PRELOAD=libm.so:libatomic.so:libcutils.so:libpython2.7.so.1.0 ./worker worker --loglevel INFO -E -n detect -c 1 -Q detect
   sleep 20
 done
