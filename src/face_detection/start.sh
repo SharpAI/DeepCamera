bc7929b14d2b832ae7f9067e9cf0737957a2c24f
Simba Zhang <solderzzc@gmail.com>
2018-12-19 20:26:39 -0800

    concurrent set to one for face detection.

--- src/face_detection/start.sh
+++ src/face_detection/start.sh
@@ -2,6 +2,6 @@
 
 while [ 1 ]
 do
-  LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib64:/data/data/com.termux/files/usr/lib python2 worker.py worker --loglevel INFO -E -n detect -c 2 -Q detect
+  LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib64:/data/data/com.termux/files/usr/lib python2 worker.py worker --loglevel INFO -E -n detect -c 1 -Q detect
   sleep 20
 done
