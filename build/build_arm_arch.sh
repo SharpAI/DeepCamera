7c11345175968375ae29ac15a57fed38e0da5b43
lambda-zhang <ggxxde@163.com>
2018-12-24 16:50:11 +0800

    update build_arm_arch.sh

--- build/build_arm_arch.sh
+++ build/build_arm_arch.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
+if [ ! $# -eq 1 ]; then
+    echo "usage: ./build_arm.sh /your/build/path"
+    exit 1
+fi
 
-runtime_tar=$1/./runtime_arch.tar
+buildpath=$(realpath $1)
 
 rm -rf build dist runtime_arch
 mkdir runtime_arch
 
-pyinstaller -y face_detector_arm.spec
+pip install -r  ../src/face_detection/requirements.txt
+pyinstaller --clean -y face_detector_arm.spec
 mv dist/worker runtime_arch/bin
 cp -rf ../src/face_detection/model runtime_arch/bin/
 
-rm $runtime_tar
-tar -cf $runtime_tar runtime_arch
-sync
-rm -rf runtime_arch/bin
-
-pyinstaller -y classifier_server.spec
+pip install -r ../src/embedding/requirements.txt
+pyinstaller --clean -y classifier_server.spec
+cp ../src/embedding/judgeutil.so_termux_arch-linux_arm dist/classifier/judgeutil.so
 mv dist/classifier runtime_arch/bin
-
-tar -uf $runtime_tar runtime_arch
-
-echo "$runtime_tar is ready"
