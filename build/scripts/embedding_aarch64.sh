c929bd06213b098d83ac23ef394fb2ec15cb1ee7
rick0626 <923939010@qq.com>
2018-12-28 08:38:41 +0000

    modify environment var

--- build/scripts/embedding_aarch64.sh
+++ build/scripts/embedding_aarch64.sh
@@ -4,6 +4,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib64
 export DATA_RUNTIME_FOLDER=../model
 export DEVICE_UUID_FILEPATH=/data/data/com.termux/files/home/.ro_serialno
 export DEVICE_GROUP_ID_FILEPATH=/data/data/com.termux/files/home/.groupid.txt
+export RUNTIME_BASEDIR=/data/data/com.termux/files/data
 cd bin
 while [ 1 ]
 do
