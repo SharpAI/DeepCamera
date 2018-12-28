68dc4060866870ed71d8909a8819e89a9d1cf0d4
rick0626 <rick.li_@outlook.com>
2018-12-28 09:03:52 +0000

    tweak a little

--- src/detector/start.sh
+++ src/detector/start.sh
@@ -2,12 +2,10 @@
 
 export DEVICE_UUID_FILEPATH=/data/data/com.termux/files/home/.ro_serialno
 export DEVICE_GROUP_ID_FILEPATH=/data/data/com.termux/files/home/.groupid.txt
-export UPLOAD_IMAGE_SERVICE_ENABLED=1
-# export RUNTIME_BASEDIR=/data/data/com.termux/files/data
 
 while [ 1 ]
 do
-  REDIS_HOST=localhost FLOWER_ADDRESS=localhost FLOWER_PORT=5556 NODE_ENV=/data/data/com.termux/files/data node index.js
+  REDIS_HOST=localhost FLOWER_ADDRESS=localhost FLOWER_PORT=5556 node index.js
   sleep 20
 done
 
