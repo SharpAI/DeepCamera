2360d83e0d38ef67c97103fe8a8ac9e09ae39b74
Simba Zhang <solderzzc@users.noreply.github.com>
2019-03-14 13:54:27 -0700

    Update embedding_aarch64.sh

--- build/scripts/embedding_aarch64.sh
+++ build/scripts/embedding_aarch64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export RUNTIME_BASEDIR=`pwd`
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib64:/system/lib64
+export LD_LIBRARY_PATH=/system/lib64:$LD_LIBRARY_PATH:$PREFIX/lib64:/system/vendor/lib64/egl:/system/vendor/lib64
 export DATA_RUNTIME_FOLDER=../model
 export DEVICE_UUID_FILEPATH=/data/data/com.termux/files/home/.ro_serialno
 export DEVICE_GROUP_ID_FILEPATH=/data/data/com.termux/files/home/.groupid.txt
@@ -11,6 +11,6 @@ export CLUSTER_CONCURRENCY=1
 cd bin
 while [ 1 ]
 do
-    LD_PRELOAD=$LD_PRELOAD:/system/lib64/libcrypto.so:/system/lib64/libcompiler_rt.so WORKER_TYPE=embedding REDIS_ADDRESS=localhost WORKER_BROKER=redis://localhost/0 ./embedding worker --loglevel INFO -E -n embedding -c 1 -Q embedding
+    WORKER_TYPE=embedding REDIS_ADDRESS=localhost WORKER_BROKER=redis://localhost/0 ./embedding worker --loglevel INFO -E -n embedding -c 1 -Q embedding
     sleep 20
 done
