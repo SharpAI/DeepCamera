5e133e8483c560df4f7bef4440dfbafc9705cfd0
Simba Zhang <solderzzc@gmail.com>
2019-03-18 16:32:57 -0700

    Fix permission issue.

--- start_service.sh
+++ start_service.sh
@@ -5,18 +5,18 @@ redis-server --maxmemory 40mb --maxmemory-policy allkeys-lru --save "" --appendo
 mosquitto &
 
 cd src/embedding
-./start_andrid_aarch64.sh &
+$PREFIX/bin/bash ./start_andrid_aarch64.sh &
 cd -
 
 cd src/face_detection
-./start_android_aarch64.sh &
+$PREFIX/bin/bash ./start_android_aarch64.sh &
 cd -
 
 cd src/detector
-./start_android_aarch64.sh &
+$PREFIX/bin/bash ./start_android_aarch64.sh &
 
 cd src/monitor/
-    ./start_android_aarch64.sh &
+    $PREFIX/bin/bash ./start_android_aarch64.sh &
 cd -
 
 while [ 1 ]
