089f15b837a146168c7a713f28d61b8ad8026783
Simba Zhang <solderzzc@gmail.com>
2019-03-18 16:39:04 -0700

    Minor change of stop_all.sh

--- stop_all.sh
+++ stop_all.sh
@@ -2,5 +2,7 @@
 
 echo Please reconnect after this script
 
-killall -9 redis-server mosquitto minio flower_main embedding worker classifier node param 
+killall -9 redis-server mosquitto minio node python2
+killall -9 flower_main embedding worker classifier param
 killall -9 bash
+
