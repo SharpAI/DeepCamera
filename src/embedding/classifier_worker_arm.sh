d47bd03ea6f134470fe749709541ea89621ae92b
Your Name <you@example.com>
2019-03-27 16:28:11 +0800

    run source code on rk3288

--- src/embedding/classifier_worker_arm.sh
+++ src/embedding/classifier_worker_arm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+cpwd=$(pwd)
 while [ 1 ]
 do
-  $PREFIX/bin/bash /data/data/com.termux/files/home/arch/startarch c "cd /data/data/com.termux/files/home/sharpai/src/embedding &&  cp -a judgeutil.so_termux_arch-linux_arm judgeutil.so &&TASKER=worker WORKER_TYPE=classify REDIS_ADDRESS=localhost DEVICE_GROUP_ID_FILEPATH=/data/data/com.termux/files/home/.groupid.txt DEVICE_UUID_FILEPATH=/data/data/com.termux/files/home/.ro_serialno RUNTIME_BASEDIR=/data/data/com.termux/files/home/sharpai/src/embedding python classifier_rest_server.py worker --loglevel INFO -E -n classify -c 1 -Q classify"
+  $PREFIX/bin/bash /data/data/com.termux/files/home/arch/startarch c "cd "${cpwd}" && TASKER=worker WORKER_TYPE=classify REDIS_ADDRESS=localhost DEVICE_GROUP_ID_FILEPATH=/data/data/com.termux/files/home/.groupid.txt DEVICE_UUID_FILEPATH=/data/data/com.termux/files/home/.ro_serialno RUNTIME_BASEDIR="${cpwd}" python classifier_rest_server.py worker --loglevel INFO -E -n classify -c 1 -Q classify"
   sleep 20
 done
