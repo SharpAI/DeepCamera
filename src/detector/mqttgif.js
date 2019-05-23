8ebff998b96313ebaad2a8b89a52ecbe9ada1a1f
Shiyi Tan <cicitan@hotmail.com>
2019-05-23 10:55:56 +0800

    Start to work on this one.

--- src/detector/mqttgif.js
+++ src/detector/mqttgif.js
@@ -3,6 +3,7 @@ var request = require('requestretry')
 var host = 'http://workaihost.tiegushi.com/'
 gst_api_url = host + 'restapi/workai'
 gst_stranger_api_url = host + 'restapi/updateStrangers'
+stranger_single_url = host + 'restapi/workai_autolabel/single'
 stranger_batch_url = host + 'restapi/workai_autolabel/batch'
 
 function GetEnvironmentVarInt(varname, defaultvalue)
@@ -102,7 +103,47 @@ function post_stranger_gif_4_label(faceid, trackerId, cameraId, gif_url, uuid, g
         }
     });
 }
+function post_stranger_single(faceid, trackerId, cameraId, gif_url, uuid, group_id, pic_arr, peoplenum){
+    console.log('POST STRANGER INFO TO SERVER...')
+    var images2lable = [];
+    if(peoplenum >= 2) {
+      images2lable.push(pic_arr[0]);
+    }
+    else {
+      images2lable = pic_arr;
+    }
 
+    var json_request_content = {
+        'imgs': images2lable,
+        'img_gif': gif_url,
+        'group_id': '' + group_id,
+        'camera_id': cameraId,
+        'uuid': uuid,
+        'tid': trackerId,
+        'isStrange': true,
+        'createTime': new Date()}
+
+    request({
+        url: gst_stranger_api_url,
+        method: "POST",
+        json: true,
+        maxAttempts: 5,   // (default) try 5 times
+        retryDelay: 5000,
+        headers: {
+        "content-type": "application/json",
+        },
+        body: json_request_content
+    }, function (error, response, body){
+        if(error) {
+            console.log("stranger---error: ",error)
+        } else {
+            console.log('stranger---body: ',body)
+            if(body && body.state=="SUCCESS" && body.result) {
+                var json = JSON.parse(body.result)
+            }
+        }
+    });
+}
 function post_stranger_batch(persons){
 
   var json_request_content = {
