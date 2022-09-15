9fce94c27a008952b5e046e2417ae6fe2da19609
Simba Zhang <solderzzc@gmail.com>
2022-09-14 17:17:27 -0700

    fix typo

--- src/home-assistant-py/build.sh
+++ src/home-assistant-py/build.sh
@@ -2,5 +2,5 @@
 # remove support of linux/arm/v6,linux/386
 # reference link: https://www.smartling.com/resources/product/building-multi-architecture-docker-images-on-arm-64-bit-aws-graviton2/
 
-echo 'Building for Nodejs backend'
+echo 'Building for Python backend'
 docker buildx build --platform linux/amd64,linux/arm64/v8,linux/arm/v7 -t shareai/home-assiistant-py:2022.8 --push . 
