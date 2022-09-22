8cc269bf9c51ea56c074a896b576011f65c75856
Simba Zhang <solderzzc@gmail.com>
2022-09-22 16:53:52 -0700

    minior change for build.sh

--- src/label-studio/build.sh
+++ src/label-studio/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/heartexlabs/label-studio -b v1.5.0 
+cp Dockerfile label-studio/
 cd label-studio
-docker buildx build --push --platform linux/amd64,linux/arm64 \
---tag sharpai/label-studio:1.5.0 .
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag shareai/label-studio:1.5.0 .
+
