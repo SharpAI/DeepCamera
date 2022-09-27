15c2bdaf167db9fe2d1253687b00a1e003820654
Simba Zhang <solderzzc@gmail.com>
2022-09-26 20:33:05 -0700

    label-studio compile successed

--- src/label-studio/build.sh
+++ src/label-studio/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-git clone https://github.com/heartexlabs/label-studio -b v1.5.0 
-cp Dockerfile label-studio/
-cd label-studio
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag shareai/label-studio:1.5.0 .
 
