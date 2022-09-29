defda92e90dacf7531aef0fdf50a6b722a457ae4
Simba Zhang <solderzzc@gmail.com>
2022-09-29 14:04:34 -0700

    added scripts to build and push images for jetson

--- src/yolov7_reid/src/build.sh
+++ src/yolov7_reid/src/build.sh
@@ -2,5 +2,8 @@
 
 docker buildx build --push --platform linux/amd64,linux/arm64 --tag shareai/yolov7_reid:latest .
 
-docker build -f Dockerfile.jetpack --build-arg JETPACK_VERSION=4 --build-arg SKIP_MINICONDA=true --build-arg UBUNTU_BASE_IMG=nvcr.io/nvidia/l4t-ml:r32.6.1-py3 --tag shareai/yolov7_reid:r32.6.1_latest .
-docker build -f Dockerfile.jetpack --build-arg JETPACK_VERSION=5.0 --build-arg SKIP_MINICONDA=true --build-arg UBUNTU_BASE_IMG=nvcr.io/nvidia/l4t-ml:r35.1.0-py3 --tag shareai/yolov7_reid:r35.1.0_latest .
+docker buildx build -f Dockerfile.jetpack --push --platform linux/arm64 --build-arg JETPACK_VERSION=4 --build-arg SKIP_MINICONDA=true --build-arg UBUNTU_BASE_IMG=nvcr.io/nvidia/l4t-ml:r32.6.1-py3 --tag shareai/yolov7_reid:r32.6.1_latest .
+docker buildx build -f Dockerfile.jetpack --push --platform linux/arm64 --build-arg JETPACK_VERSION=5.0 --build-arg SKIP_MINICONDA=true --build-arg UBUNTU_BASE_IMG=nvcr.io/nvidia/l4t-ml:r35.1.0-py3 --tag shareai/yolov7_reid:r35.1.0_latest .
+
+# docker build -f Dockerfile.jetpack --build-arg JETPACK_VERSION=4 --build-arg SKIP_MINICONDA=true --build-arg UBUNTU_BASE_IMG=nvcr.io/nvidia/l4t-ml:r32.6.1-py3 --tag shareai/yolov7_reid:r32.6.1_latest .
+# docker build -f Dockerfile.jetpack --build-arg JETPACK_VERSION=5.0 --build-arg SKIP_MINICONDA=true --build-arg UBUNTU_BASE_IMG=nvcr.io/nvidia/l4t-ml:r35.1.0-py3 --tag shareai/yolov7_reid:r35.1.0_latest .
